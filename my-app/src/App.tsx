8df6cf0e79c5a67973fb7574bbabb772e37847ab
adi518 <adi.s@openweb.com>
2020-11-29 13:02:03 +0200

    update my-app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 
-import { Button, Icon, Modal /* PopperTooltip */ } from '@spotim/ui-components';
+// import { Icon } from '@spotim/ui-components';
+// import { Modal } from '@spotim/ui-components';
+// import { Button } from '@spotim/ui-components';
+// import { PopperTooltip } from '@spotim/ui-components';
+// import { Icon } from 'ui-components-tsdx';
+// import { Modal } from 'ui-components-tsdx';
+import { Button } from 'ui-components-tsdx';
 
 function App() {
   return (
     <div className="App" id="spotim-specific" data-spot-im-direction="ltr">
       <header className="App-header">
-        {/* <Modal isLtr={true} onClose={() => {}} showModal={true}></Modal> */}
-        {/* <PopperTooltip></PopperTooltip> */}
         <Button mode="primary">Button</Button>
-        <Icon name="arrow" type="fa-regular"></Icon>
+        {/* <PopperTooltip></PopperTooltip> */}
+        {/* <Icon name="arrow" type="fa-regular"></Icon> */}
+        {/* <Modal isLtr={true} onClose={() => {}} showModal={true}></Modal> */}
       </header>
     </div>
   );
